440efd62fcc145651b66ac8f78120205fb946b26
vinhbk2000 <vinhbk2000@gmail.com>
2019-06-02 17:55:56 +0700

    change name

--- db_chart_source/db_chart_source/dashboard_chart_source/profit_and_loss_statement/profit_and_loss_statement.js
+++ db_chart_source/db_chart_source/dashboard_chart_source/profit_and_loss_statement/profit_and_loss_statement.js
@@ -1,4 +1,4 @@
-frappe.dashboard_chart_sources["Account Balance Report"] = {
+frappe.dashboard_chart_sources["Profit and Loss Statement"] = {
 	"method_path": "db_chart_source.db_chart_source.dashboard_chart_source.profit_and_loss_statement.profit_and_loss_statement.get",
 	"filters": [
 		{
